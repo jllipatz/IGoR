a27e56062a1d5c9cdffc88db6201bde965cc17a4
jllipatz <jean-luc.lipatz@insee.fr>
2019-07-31 11:15:44 +0200

    Add files via upload
    1.03.1

--- dashboard.R
+++ dashboard.R
@@ -5,7 +5,7 @@
   
   header = dashboardHeader(
     title = "I Go R"
-    ,tags$li(class = "dropdown", p("version 1.03.0"))
+    ,tags$li(class = "dropdown", p(em("version 1.03.1")))
     ,tags$li(a(href = 'http://www.insee.fr',
                img(src = 'images/logo_insee.png',
                    title = "insee.fr", height = "46px"),
